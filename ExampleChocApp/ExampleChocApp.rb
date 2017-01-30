58a2b48dd5c14c7974ed845f62b929a2cc4e3adc
Alex Moore-Niemi <alex.moore-niemi@raizlabs.com>
2017-01-30 16:33:53 -0500

    skeleton of generated app

---
+++ ExampleChocApp/ExampleChocApp.rb
@@ -0,0 +1,56 @@
+require 'sinatra'
+
+get 'homepage' do
+end
+
+get 'product' do
+end
+
+get 'categories' do
+end
+
+get 'vendors' do
+end
+
+get 'categories/:category_id' do
+end
+
+get 'products/:product_id' do
+end
+
+get 'products?by_rating' do
+end
+
+get 'products/:product_id/reviews' do
+end
+
+get 'reviews/:review_id' do
+end
+
+post 'review' do
+end
+
+put 'review' do
+end
+
+post 'cart' do
+end
+
+delete 'cart' do
+end
+
+post 'invoice' do
+end
+
+get 'product/:vendor_id' do
+end
+
+get 'vendors/:vendor_id' do
+end
+
+get 'products' do
+end
+
+get 'root' do
+end
+
